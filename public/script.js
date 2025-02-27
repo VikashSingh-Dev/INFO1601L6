437d47399edc516091233b3946db4565fea370a7
VikashSingh-Dev <vikashsinghhvc@gmail.com>
2025-02-27 19:17:22 -0400

    pre task 5
    pre task 5 content

--- public/script.js
+++ public/script.js
@@ -1,20 +1,49 @@
-//get client data using navigator
-console.log(window.navigator.cookieEnabled);
-console.log(window.navigator.onLine);
-console.log(navigator.appVersion);
-console.log(navigator.userAgent)
-console.log(navigator.platform);
+let bob = {
+    name: 'Bob',
+    balance: 10
+  }
+  
+  function add10(a){
+    a.balance+= 10; // a is a pointer/reference to myObj
+  }
+  
+  console.log(bob.balance);//10
+  add10(bob);
+  console.log(bob.balance);//20
 
-//get window metadata using window
-console.log(window.location.href);//get full url
-console.log(window.location.protocol);
-console.log(window.location.hostname);
+  let myobj = {name: 'John', age: 31, city: 'New York'};
+  let obj2 = myobj;
 
-function redirect(url){
-  window.location.assign(url);//redirects the page to another url
-}
+  //obj2 and myobj are the same variable
+  
+  obj2.name = "Shelly";
+  
+  console.log(myobj.name);//Shelly
+  
+  //if you need to create a new object and copy its values use Object.assign
+  
+  let obj3 = {};
+  Object.assign(obj3, myobj);
+  
+  obj3.name = 'Smith';
+  
+  console.log(myobj.name, obj3.name);//Shelly, Smith
 
-window.onload = function(event){
-   console.log("Page has loaded");
-   //do other javascript stuff here
-}
+
+  // task 5
+  let obj2 = myobj;
+
+  //obj2 and myobj are the same variable
+  
+  obj2.name = "Shelly";
+  
+  console.log(myobj.name);//Shelly
+  
+  //if you need to create a new object and copy its values use Object.assign
+  
+  let obj3 = {};
+  Object.assign(obj3, myobj);
+  
+  obj3.name = 'Smith';
+  
+  console.log(myobj.name, obj3.name);//Shelly, Smith
